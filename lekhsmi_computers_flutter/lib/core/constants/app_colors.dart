286c0bbe310e373fcfd742c4e3d03ab22db4fca9
abijithb07051999 <abijithb07051999@gmail.com>
2026-07-24 15:09:59 +0530

    feat: added constant colors and icons

---
+++ lekhsmi_computers_flutter/lib/core/constants/app_colors.dart
@@ -0,0 +1,13 @@
+class AppColors {
+  static const PRIMARY = 0xFF247CEB;
+  static const SECONDAY = 0xFFE31D7F;
+  static const SUCCESS = 0xFF22C55E;
+  static const WARNING = 0xFFF59E0B;
+  static const ERROR = 0xFFEF4444;
+  static const INFO = 0xFF0EA5E9;
+  static const BACKGROUND = 0xFFF8FAFC;
+  static const WHITE = 0xFFFFFFFF;
+  static const TEXTPRIMARY = 0xFF1F2937;
+  static const TEXTSECONDARY = 0xFF6B7280;
+  static const TEXTLIGHT = 0xFF6B7280;
+}
---
+++ lekhsmi_computers_flutter/lib/core/constants/app_icons.dart
@@ -0,0 +1,52 @@
+import 'package:hugeicons/hugeicons.dart';
+
+class AppIcons {
+  static const DASHBOARDICON = HugeIcons.strokeRoundedDashboardSquare02;
+  static const INVENTORYICON = HugeIcons.strokeRoundedDeliveryBox02;
+  static const ORDERICON = HugeIcons.strokeRoundedClipboardClock;
+  static const ACCOUNSTICON = HugeIcons.strokeRoundedWallet03;
+  static const QUOTATIONICON = HugeIcons.strokeRoundedGoogleDoc;
+  static const INVOICEICON = HugeIcons.strokeRoundedInvoice02;
+  static const SETTINGSICON = HugeIcons.strokeRoundedSettings01;
+
+  static const ADDPERSONICON = HugeIcons.strokeRoundedAddMale;
+  static const ADDICON = HugeIcons.strokeRoundedAddCircle;
+
+  static const EDITICON = HugeIcons.strokeRoundedPencilEdit02;
+  static const DELETEICON = HugeIcons.strokeRoundedDelete02;
+
+  static const CHECKICON = HugeIcons.strokeRoundedCheckmarkCircle01;
+  static const PARTIALICON = HugeIcons.strokeRoundedSplit;
+  static const CANCELICON = HugeIcons.strokeRoundedCancelCircle;
+
+  static const VIEWICON = HugeIcons.strokeRoundedView;
+
+  static const PRODUCTADDICON = HugeIcons.strokeRoundedShoppingCartAdd02;
+  static const PURCHASEADDICON = HugeIcons.strokeRoundedPackageAdd01;
+
+  static const SAVEICON = HugeIcons.strokeRoundedSave;
+
+  static const CALENDARICON = HugeIcons.strokeRoundedCalendar03;
+
+  static const HISTORYICON = HugeIcons.strokeRoundedTransactionHistory;
+
+  static const MONEYRECEIVEICON = HugeIcons.strokeRoundedMoneyReceiveCircle;
+  static const MONEYSENDICON = HugeIcons.strokeRoundedMoneySendCircle;
+  static const PROFITICON = HugeIcons.strokeRoundedProfit;
+
+  static const GRAPHUPICON = HugeIcons.strokeRoundedTradeUp;
+  static const GRAPHDOWNICON = HugeIcons.strokeRoundedTradeDown;
+
+  static const PRINTERICON = HugeIcons.strokeRoundedPrinter;
+
+  static const USERICON = HugeIcons.strokeRoundedUser03;
+  static const PHONEICON = HugeIcons.strokeRoundedCall02;
+  static const EMAILICON = HugeIcons.strokeRoundedMail01;
+  static const WEBSITEICON = HugeIcons.strokeRoundedGlobe02;
+  static const HOMEICON = HugeIcons.strokeRoundedHome07;
+
+  static const TOTALINCOMEICON = HugeIcons.strokeRoundedSaveMoneyDollar;
+  static const BRANDSCOUNTICON = HugeIcons.strokeRoundedTag01;
+  static const CATEGORYCOUNTICON = HugeIcons.strokeRoundedDashboardSquare01;
+  static const SUPPLIERCOUNTICON = HugeIcons.strokeRoundedUserGroup03;
+}
